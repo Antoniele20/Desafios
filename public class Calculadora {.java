a4b9ae43b5017a079d175bd74f606abf19f5983c
Antoniele20 <144742273+Antoniele20@users.noreply.github.com>
2024-04-25 19:34:01 -0300

    Update public class Calculadora {.java

--- public class Calculadora {.java
+++ public class Calculadora {.java
@@ -1,18 +1,1 @@
-public class Biblioteca {
-    String Titulo;
-    String Autor;
-    float Ano;
 
-    Aluno (String ti, String aut,Float an) {
-        System.out.println("~ novo livro adicionado - " + ti + " ~");
-        Titulo = ti;
-        Autor = aut;
-        Ano = an;
-    
-        if(mostrar){
-        System.out.println("Nome do livro: " + Titulo);
-        System.out.println("Autor" + Autor);
-        System.out.println("Ano de publicação: " + Ano);
-    }
-}
-}
